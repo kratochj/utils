7bd10611bc6f1a877ce19a41a1abadb5dea32309
Jiri Kratochvil <jiri.kratochvil@topmonks.com>
2013-11-04 11:14:20 +0100

    Formatted JAvaDoc

--- utils-rt/src/main/java/eu/kratochvil/util/JsonFormatter.java
+++ utils-rt/src/main/java/eu/kratochvil/util/JsonFormatter.java
@@ -11,7 +11,11 @@ import org.json.simple.JSONObject;
  * <code><pre>
  *     public static void main(final String[] args) throws JSONException{
  *          final JSONObject obj =
- *          new JSONObject("{\"glossary\":{\"title\": \"example glossary\", \"GlossDiv\":{\"title\": \"S\", \"GlossList\":{\"GlossEntry\":{\"ID\": \"SGML\", \"SortAs\": \"SGML\", \"GlossTerm\": \"Standard Generalized Markup Language\", \"Acronym\": \"SGML\", \"Abbrev\": \"ISO 8879:1986\", \"GlossDef\":{\"para\": \"A meta-markup language, used to create markup languages such as DocBook.\", \"GlossSeeAlso\": [\"GML\", \"XML\"]}, \"GlossSee\": \"markup\"}}}}}");
+ *          new JSONObject("{\"glossary\":{\"title\": \"example glossary\", \"GlossDiv\":{\"title\": \"S\", " +
+ *              "\"GlossList\":{\"GlossEntry\":{\"ID\": \"SGML\", \"SortAs\": \"SGML\", \"GlossTerm\": " +
+ *              "\"Standard Generalized Markup Language\", \"Acronym\": \"SGML\", \"Abbrev\": \"ISO 8879:1986\", " +
+ *              "\"GlossDef\":{\"para\": \"A meta-markup language, used to create markup languages such as DocBook.\", " +
+ *              "\"GlossSeeAlso\": [\"GML\", \"XML\"]}, \"GlossSee\": \"markup\"}}}}}");
  *          System.out.println(JsonFormatter.format(obj));
  *      }
  * </pre></code>
